a9b7a734e72786ac88549ae4d688655e2e30d583
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-02-26 03:11:16 -0300

    learn(exercicio03-react): define nomes dos medicos em um objeto JSON

--- exercicio-03/src/App.jsx
+++ exercicio-03/src/App.jsx
@@ -8,20 +8,26 @@ function App() {
       return {width: 1200, margin: 'auto', border: '1px solid black',
      backgroundColor: "#EEE", borderRadius: 6, padding: 10, textAlign: 'center'};
       }
+
+      const NomesMedicos = {medico1: 'José da Silva', medico2: 'Maria da Silva', medico3: 'Jaqueline Mendes'};
+
       return (
       <div style={estiloAreaDocs()}>
       <h2>Profissionais de saúde</h2>
       <div style={{margin: 7, border: '1px solid #DDD', borderRadius: 8, padding: 60, display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
       
       <div className="profissionais">
+         <p>{NomesMedicos.medico1}</p>
 
    </div>
 
       <div className="profissionais">
+         <p>{NomesMedicos.medico2}</p>
 
    </div>
 
       <div className="profissionais">
+         <p>{NomesMedicos.medico3}</p>
 
    </div>
 
