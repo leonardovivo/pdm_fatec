9deebcc1a41ee2ca6efac08bfc3c768c5e5182b5
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-02-26 03:58:45 -0300

    Remove contador do projeto padrao vite e muda nome da variavel para portugues

--- exercicio-03/src/App.jsx
+++ exercicio-03/src/App.jsx
@@ -3,9 +3,8 @@ import medico1 from './images/medico1.jpg'
 import './styles.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-    const estiloAreaDocs = () => {
+    const estiloAreaMedicos = () => {
       return {width: 1200, margin: 'auto', border: '1px solid black',
      backgroundColor: "#EEE", borderRadius: 6, padding: 10, textAlign: 'center'};
       }
@@ -15,7 +14,7 @@ function App() {
       const medico2 = import.meta.env.VITE_IMAGEM_MEDICO2;
 
       return (
-      <div style={estiloAreaDocs()}>
+      <div style={estiloAreaMedicos()}>
       <h2>Profissionais de saúde</h2>
       <div style={{margin: 7, border: '1px solid #DDD', borderRadius: 8, padding: 60, display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
       
