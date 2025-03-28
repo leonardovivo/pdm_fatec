a709dd48535104493259e58e0dd47e0f5f921e39
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-03-28 17:09:58 -0300

    learn(ap07-react): encerra a pagina 17

---
+++ exibe-fotos/public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+    "PEXELS_KEY": "ZIrxAioiLaN6O3yuDeQ0rNsqtkr9OZ1AzU4CVqXgPRJ4LDbOiPOx3wGM"
+  };
--- exibe-fotos/src/components/App.jsx
+++ exibe-fotos/src/components/App.jsx
@@ -1,14 +1,27 @@
+// rafce
 import React from 'react'
 import Busca from './Busca'
 
+class App extends React.Component {
 
-const App = () => {
-  return (
-    <div>
-        <h1>Exibir uma lista de...</h1>
-        <Busca />
-    </div>
-  )
+  onBuscaRealizada = (termo) => {
+    console.log(termo)
+  }
+
+  render(){
+    return (
+      <div className='grid justify-content-center'>
+          <div className="col-12">
+            <h1 className='text-center'>Exibir uma lista de...</h1>
+          </div>
+          <div className="col-12">
+            <Busca 
+              dica="Digite algo que deseja ver..."
+              onBuscaRealizada={this.onBuscaRealizada}/>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App
